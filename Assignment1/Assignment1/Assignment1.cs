0579595d1d6c679b6b3490f1acbb5904af321507
DESKTOP-Q5L4SNM\Adminstrator <leejaemin9854@daum.net>
2023-05-28 01:40:31 +0900

    no message

--- Assignment1/Assignment1/Assignment1.cs
+++ Assignment1/Assignment1/Assignment1.cs
@@ -50,15 +50,15 @@ namespace Assignment1
             f3 += Convert.ToString(width);
 
             fmat = f1 + "} " + f2 + "} " + f3 + "}";
-            string[] nums = new string[5];
+            double[] nums = new double[5];
             for (int i = 0; i < 5; i++)
             {
-                nums[i] = input.ReadLine();
+                nums[i] = double.Parse(input.ReadLine());
             }
             output.WriteLine(fmat, "oct", "dec", "hex");
             for (int i = 0; i < 5; i++)
             {
-                output.WriteLine(fmat, NumberConvert(Convert.ToString(double.Parse(nums[i])), 8), nums[i], NumberConvert(Convert.ToString(double.Parse(nums[i])), 16));
+                output.WriteLine(fmat, NumberConvert(Convert.ToString(nums[i]), 8), nums[i], NumberConvert(Convert.ToString(nums[i]), 16));
             }
 
         }
