9278b255dcd3cd166366fdcbbaa2f971a81b5aa1
DESKTOP-Q5L4SNM\Adminstrator <leejaemin9854@daum.net>
2023-05-27 18:20:48 +0900

    no message

--- Assignment1/Assignment1/Program.cs
+++ Assignment1/Assignment1/Program.cs
@@ -7,7 +7,7 @@ namespace Assignment1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Write");
+            Console.WriteLine("Hello World");
         }
     }
 }
