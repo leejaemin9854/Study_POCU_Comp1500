a0f5343431b7b43dfa2511fd6c32d6cf0a6c37e6
DESKTOP-Q5L4SNM\Adminstrator <leejaemin9854@daum.net>
2023-06-03 15:11:20 +0900

    no message

--- Lab5/Lab5/Lab5.cs
+++ Lab5/Lab5/Lab5.cs
@@ -12,7 +12,7 @@ namespace Lab5
         {
             bool bResult = false;
             int index = 0;
-            double rightValue = 0;
+            decimal rightValue = 0;
 
             if (usersPerDay.Length != revenuePerDay.Length)
                 return false;
@@ -22,29 +22,29 @@ namespace Lab5
             {
                 if (usersPerDay[index] <= 10)
                 {
-                    rightValue = (double)usersPerDay[index] / 2;
+                    rightValue = (decimal)usersPerDay[index] / 2;
 
                 }
                 else if (usersPerDay[index] <= 100)
                 {
-                    rightValue = 16 * (double)usersPerDay[index] / 5 - 27;
+                    rightValue = 16 * (decimal)usersPerDay[index] / 5 - 27;
 
                 }
                 else if (usersPerDay[index] <= 1000)
                 {
-                    rightValue = (double)usersPerDay[index] * (double)usersPerDay[index] / 4 - 2 * (double)usersPerDay[index] - 2007;
+                    rightValue = (decimal)usersPerDay[index] * (decimal)usersPerDay[index] / 4 - 2 * (decimal)usersPerDay[index] - 2007;
 
                 }
                 else
                 {
-                    rightValue = 245743 + (double)usersPerDay[index] / 4;
+                    rightValue = 245743 + (decimal)usersPerDay[index] / 4;
 
                 }
 
 
-                if (rightValue != revenuePerDay[index])
+                if ((float)rightValue != (float)revenuePerDay[index])
                 {
-                    revenuePerDay[index] = rightValue;
+                    revenuePerDay[index] = (double)rightValue;
                     bResult = true;
                 }
             }
@@ -56,8 +56,8 @@ namespace Lab5
         public static int GetInvalidEntryCount(uint[] usersPerDay, double[] revenuePerDay)
         {
             int result = 0;
-            double rightValue = 0;
-            uint i;
+            decimal rightValue = 0;
+            int i;
 
             if (usersPerDay.Length != revenuePerDay.Length || usersPerDay.Length == 0) 
                 return -1;
@@ -65,28 +65,28 @@ namespace Lab5
 
             for (i = 0; i < revenuePerDay.Length; i++)
             {
-                if (usersPerDay[i] <= 10)
+                if (usersPerDay[i] <= 10f)
                 {
-                    rightValue = (double)usersPerDay[i] / 2;
+                    rightValue = (decimal)usersPerDay[i] / 2;
 
                 }
-                else if (usersPerDay[i] <= 100)
+                else if (usersPerDay[i] <= 100f)
                 {
-                    rightValue = 16f * (double)usersPerDay[i] / 5 - 27;
+                    rightValue = 16 * (decimal)usersPerDay[i] / 5 - 27;
 
                 }
-                else if (usersPerDay[i] <= 1000)
+                else if (usersPerDay[i] <= 1000f)
                 {
-                    rightValue = (double)usersPerDay[i] * (double)usersPerDay[i] / 4 - 2 * (double)usersPerDay[i] - 2007;
+                    rightValue = (decimal)usersPerDay[i] * (decimal)usersPerDay[i] / 4 - 2 * (decimal)usersPerDay[i] - 2007;
                     
                 }
                 else
                 {
-                    rightValue = 245743 + (double)usersPerDay[i] / 4;
+                    rightValue = 245743 + (decimal)usersPerDay[i] / 4;
 
                 }
 
-                if (rightValue != revenuePerDay[i])
+                if (rightValue != (decimal)revenuePerDay[i])
                     result++;
             }
 
--- Lab5/Lab5/Program.cs
+++ Lab5/Lab5/Program.cs
@@ -11,4 +11,3 @@ namespace Lab5
         }
     }
 }
-
