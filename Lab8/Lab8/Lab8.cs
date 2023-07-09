8e3d382b2ff3a8eeb5e19d1d4ee01ad14d23cab6
DESKTOP-Q5L4SNM\Adminstrator <leejaemin9854@daum.net>
2023-07-09 19:00:52 +0900

    no message

--- Lab8/Lab8/Lab8.cs
+++ Lab8/Lab8/Lab8.cs
@@ -8,32 +8,7 @@ namespace Lab8
 {
     internal class Lab8
     {
-        public static int test(string s1, string s2)
-        {
-            string p = s1.Length >= s2.Length ? s2 : s1;
-
-            for (int i = 0; i < p.Length; i++)
-            {
-                if (s1[i] != s2[i])
-                {
-                    Console.WriteLine($"B: s1[{i-1}]: {(int)s1[i-1]}");
-                    Console.WriteLine($"B: s2[{i-1}]: {(int)s2[i-1]}");
-
-                    Console.WriteLine($"s1[{i}]: {(int)s1[i]}");
-                    Console.WriteLine($"s2[{i}]: {(int)s2[i]}");
-
-                    Console.WriteLine($"A: s1[{i + 1}]: {(int)s1[i + 1]}");
-                    Console.WriteLine($"A: s2[{i + 1}]: {(int)s2[i + 1]}");
-
-                    return i;
-                } 
-            }
-            Console.WriteLine("Same");
-            return -1;
-
-        }
-
-
+        
         public static string ChangeToAlpha(uint num)
         {
             StringBuilder s = new StringBuilder(128);
@@ -62,7 +37,7 @@ namespace Lab8
             string addString;
             string nLine = "\n";
 
-            char[] delims_lv = { '|', '_', '/' };
+            char[] delimsLv = { '|', '_', '/' };
             uint[] index = { 0, 0, 0 };
 
             addString = $"{++index[0]}) ";
@@ -70,7 +45,7 @@ namespace Lab8
 
             foreach (char c in s)
             {
-                if (c == delims_lv[0])
+                if (c == delimsLv[0])
                 {
 
                     addString = $"{nLine}{index[0] + 1}) ";
@@ -81,7 +56,7 @@ namespace Lab8
                     index[1] = 0;
                     index[2] = 0;
                 }
-                else if (c == delims_lv[1])
+                else if (c == delimsLv[1])
                 {
                     index[2] = 1;
 
@@ -91,11 +66,11 @@ namespace Lab8
 
                     index[1]++;
                 }
-                else if (index[2] == 1 && c == delims_lv[2]) 
+                else if (index[2] == 1 && c == delimsLv[2]) 
                 {
                     addString = $"{nLine}        - ";
 
-                    result+=addString;
+                    result += addString;
                 }
                 else
                 {
