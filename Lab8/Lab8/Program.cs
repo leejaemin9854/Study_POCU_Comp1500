003ab52874bc2c5256164b252d1fa0cc4fd2aa77
DESKTOP-Q5L4SNM\Adminstrator <leejaemin9854@daum.net>
2023-07-14 03:53:18 +0900

    no message

--- Lab8/Lab8/Program.cs
+++ Lab8/Lab8/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Lab8
 {
@@ -6,7 +7,9 @@ namespace Lab8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.Write(Lab8.PrettifyListOrNull("\r"));
+
         }
     }
 }
---
+++ Lab9/Lab9/Lab9.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9
+{
+    internal class Lab9
+    {
+        public static List<int> MergeLists(List<int> sortedList1, List<int> sortedList2)
+        {
+            List<int> result = new List<int>(sortedList1.Count + sortedList2.Count);
+
+            foreach (int num in sortedList1)
+            {
+                result.Add(num);
+            }
+            
+            foreach (int num in sortedList2)
+            {
+                int index = 0;
+
+                for (index = 0; index < result.Count; index++)
+                {
+                    if (result[index] > num)
+                    {
+                        break;
+                    }
+                }
+                result.Insert(index, num);
+
+            }
+
+
+            return result;
+        }
+
+        public static Dictionary<string, int> CombineListsToDictionary(List<string> keys, List<int> values)
+        {
+            Dictionary<string, int> result = new Dictionary<string, int>();
+
+            int size = keys.Count <= values.Count ? keys.Count : values.Count;
+            int index = 0;
+
+            for (index = 0; index < size; index++)
+            {
+                if (!result.ContainsKey(keys[index]))
+                {
+                    result.Add(keys[index], values[index]);
+                }
+
+            }
+
+
+            return result;
+        }
+
+        public static Dictionary<string, decimal> MergeDictionaries(Dictionary<string, int> numerators, Dictionary<string, int> denominators)
+        {
+            Dictionary<string, decimal> result = new Dictionary<string, decimal>();
+
+            if (numerators.Count > 0 && denominators.Count > 0)
+            {
+                foreach (KeyValuePair<string, int> numer in numerators)
+                {
+                    if (denominators.ContainsKey(numer.Key) && denominators[numer.Key] != 0) 
+                    {
+                        decimal va = (decimal)numer.Value / (decimal)denominators[numer.Key];
+
+                        result.Add(numer.Key, va < 0 ? va * -1 : va);
+
+                    }
+
+                }
+
+            }
+
+
+
+            return result;
+        }
+
+    }
+}
